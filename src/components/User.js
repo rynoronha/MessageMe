63ad1f213e44f383e9ce4eff767579eee0369bc5
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-04-11 10:09:43 -0400

    Assignment 5 completed

--- src/components/User.js
+++ src/components/User.js
@@ -5,9 +5,6 @@ class User extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      signedOn: false
-    };
 
     this.signIn = this.signIn.bind(this);
     this.signOut = this.signOut.bind(this);
@@ -18,13 +15,11 @@ class User extends Component {
     signIn() {
       const provider = new this.props.firebase.auth.GoogleAuthProvider();
       this.props.firebase.auth().signInWithPopup( provider );
-      this.setState({ signedOn: true});
     }
 
     signOut() {
       this.props.firebase.auth().signOut();
       this.props.setUser(null);
-      this.setState({ signedOn: false});
     }
 
     componentDidMount() {
@@ -34,7 +29,7 @@ class User extends Component {
     }
 
     buttonDisplay() {
-      if (this.state.signedOn === false) {
+      if (this.props.username === "Guest" || this.props.username === null ) {
         return <Button id="user-sign-in" bsStyle="success" onClick={this.signIn}>Sign-In</Button>
       } else
         return <Button id="user-sign-out" bsStyle="danger" onClick={this.signOut}>Sign-Out</Button>
